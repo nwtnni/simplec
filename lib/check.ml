41afd6a185ae84337882d37761bfbf93c9e232c4
Newton Ni <nwtnni@gmail.com>
2018-11-16 20:57:50 -0500

    Implement type-checking for types, unary operators, and binary operators

--- lib/check.ml
+++ lib/check.ml
@@ -1,6 +1,7 @@
 open Core
 open Result
 open Types
+open Typed
 
 type error =
 | Expected of (Typed.t * (Typed.t * Span.t))
@@ -21,12 +22,40 @@ module Env = struct
     | Some (_, t) -> Ok t
 end
 
+let rec check_type (t: Type.t) : Typed.t =
+  let open Type in match fst t with
+  | Int -> Int
+  | Bool -> Bool
+  | Unit -> Unit
+  | Fun (l, r) -> Fun (check_type l, check_type r)
+  | Prod (l, r) -> Prod (check_type l, check_type r)
+  | Sum (l, r) -> Sum (check_type l, check_type r)
+
+let check_bin (op: Bin.t) : Typed.t =
+  let open Bin in match fst op with
+  | Add
+  | Sub
+  | Mul
+  | Div -> Fun (Int, Fun (Int, Int))
+  | LAnd
+  | LOr -> Fun (Bool, Fun (Bool, Bool))
+  | Lt
+  | Le
+  | Ge
+  | Gt
+  | Eq
+  | Ne -> Fun (Int, Fun (Int, Bool))
+
+let check_uno (op: Uno.t) : Typed.t =
+  let open Uno in match fst op with
+  | Neg -> Fun (Int, Int)
+  | Not -> Fun (Bool, Bool)
+
 let check_exp (env: Env.t) (e: Exp.t) : (Typed.t, error) result =
-  let open Exp in
-  match fst e with
-  | Int _ -> Ok Typed.Int
+  let open Exp in match fst e with
+  | Int _ -> Ok Int
   | True
-  | False -> Ok Typed.Bool
-  | Unit  -> Ok Typed.Unit
+  | False -> Ok Bool
+  | Unit  -> Ok Unit
   | Var v -> Env.find env v
   | _ -> failwith "unimplemented"
