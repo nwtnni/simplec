405f3720892b79072e7414f510800d2b7e011a39
Newton Ni <nwtnni@gmail.com>
2018-11-16 19:50:55 -0500

    Implement driver using Core.Command

--- bin/simplec.ml
+++ bin/simplec.ml
@@ -1,18 +1,49 @@
 open Simple
+open Core
 
 exception ParseError of string
 
-type mode =
-| Parse
-| Type
+type target =
+| Dir of string
+| File of string
 
-let () =
-  let file = open_in (Sys.argv.(1)) in
-  let lexbuf = Lexing.from_channel file in
-  try
-    let program = Parse.program Lex.token lexbuf in
-    Print.Exp.format_t Format.std_formatter program;
-    Format.print_flush ();
-    print_newline ()
+let target = Command.Arg_type.create
+  begin fun file ->
+    if Sys.is_file file = `Yes then File file else
+    if Sys.is_directory file = `Yes then Dir file else
+    (eprintf "Could not find %s." file; exit 1)
+  end
+
+let parse file =
+  let lexbuf = file
+  |> In_channel.create ~binary:false 
+  |> Lexing.from_channel
+  in try lexbuf
+  |> Parse.program Lex.token
+  |> Print.Exp.format_t Format.std_formatter
+  |> Format.print_newline
   with
   | Parse.Error -> raise (ParseError (Span.to_string (Lex.span lexbuf)))
+
+let driver early target =
+  match target with
+  | File file -> parse file
+  | Dir dir ->
+    Sys.chdir dir;
+    let go halt file =
+      if file = "." || file = ".." || halt then false
+      else try parse file; false with
+      | ParseError span -> eprintf "%s" span; early
+    in
+    let _ = Sys.fold_dir ~init:false ~f:go (Sys.getcwd ()) in ()
+
+let () =
+  let open Command.Let_syntax in
+  Command.basic
+    ~summary:"Compiler front-end for the simply-typed lambda calculus"
+    [%map_open
+      let early = flag "early" no_arg ~doc:"halt after first error"
+      and target = anon ("FILE" %: target) in
+      fun () -> driver early target
+    ]
+  |> Command.run
