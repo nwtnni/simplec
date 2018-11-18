0fd7862c9dade5111de56feeb97c4f912dfb3a07
Newton Ni <nwtnni@gmail.com>
2018-11-17 23:57:08 -0500

    Use recursive modules for cyclic dependency between value and env

--- lib/eval.ml
+++ lib/eval.ml
@@ -1,17 +1,35 @@
 open Core
+open Types
 
-type value =
-| Int of int 
-| Bool of bool
-| Unit of unit
-| Prod of value * value
-| Left of value 
-| Right of value
+module rec Value : sig 
+  type t =
+  | Int of int 
+  | Bool of bool
+  | Unit of unit
+  | Prod of t * t
+  | Left of t 
+  | Right of t
+  | Fun of Var.t * Env.t * Exp.t
+end = struct
+  include Value
+end
 
-module Binding = struct
-  type t = value
+and Binding : sig
+  type t = Value.t
   type error = never_returns
+  val unbound: Var.t -> error
+end = struct
+  include Binding
   let unbound _ = failwith "[INTERNAL ERROR]: problem with type-checking" 
 end
 
-module Env = Env.Make (Binding)
+and Env : sig 
+  type t = (Var.t * Value.t) list
+  val empty: t
+  val insert: t -> Var.t -> Value.t -> t
+  val find: t -> Var.t -> (Value.t, never_returns * Span.t) result
+end = Simple.Env.Make (Binding)
+
+let rec eval_exp e env =
+  match e with
+  | _ -> failwith "unimplemented"
